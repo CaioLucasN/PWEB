ec127114be217224f63d00674a6ae5454ab10c09
CaioLucasN <88117554+CaioLucasN@users.noreply.github.com>
2021-10-06 22:33:46 -0300

    Create Atividade10.js

---
+++ Atividade10/Atividade10.js
@@ -0,0 +1,12 @@
+var Aluno1 = new Object();
+Aluno1.RA = "0030481923004";
+Aluno1.Nome = "Caio Lucas";
+
+var Aluno2 = {};
+Aluno1.RA = "0030481923400";
+Aluno1.Nome = "Lucas Caio";
+
+var Aluno3 = {
+  RA: "0030481923004",
+  Nome: "Caio Lucas"
+}
