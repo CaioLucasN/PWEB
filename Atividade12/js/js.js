118c7828a1f92dae1f8c4ccd8995787c96ba699e
CaioLucasN <88117554+CaioLucasN@users.noreply.github.com>
2021-11-02 20:25:21 -0300

    Add files via upload

---
+++ Atividade12/js/js.js
@@ -0,0 +1,11 @@
+function mouseOn(obj) {
+  document.getElementById("img1").src = "images/janelaaberta.png";
+}
+
+function mouseOut(obj) {
+  document.getElementById("img1").src = "images/janelafechada.png";
+}
+
+function onClick(obj) {
+  document.getElementById("img1").src = "images/janelaquebra.png";
+}
